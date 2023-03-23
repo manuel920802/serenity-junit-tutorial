006da84b4bfbc5660a8839a0cb23f6027aa71bdc
manuel76046 <kenseith92@hotmail.com>
2023-03-23 17:22:18 -0500

    added login test

--- src/test/java/serenityswag/authentication/actions/LoginActions.java
+++ src/test/java/serenityswag/authentication/actions/LoginActions.java
@@ -1,9 +1,11 @@
 package serenityswag.authentication.actions;
 
 import net.serenitybdd.core.steps.UIInteractionSteps;
+import net.thucydides.core.annotations.Step;
 
 public class LoginActions extends UIInteractionSteps {
 
+    @Step("Log in as a standard user")
     public void asAStandardUser() {
         openUrl("https://www.saucedemo.com/");
         //$ = find
